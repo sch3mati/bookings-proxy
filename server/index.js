e9878d1505fff3102058126c864c60c344a5bb2b
Johnny Tang <email.john.e@gmail.com>
2020-10-05 13:53:45 -0700

    setup server and html

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, '/../public')));
+
+const port = 3005;
+
+app.listen(port, () => {
+  console.log(`Listening at localhost:${port}`);
+});
