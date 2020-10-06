044326d5826bd15b8af03527ebee1944ef3e9159
Johnny Tang <email.john.e@gmail.com>
2020-10-06 16:09:29 -0700

    render components on single page

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 
@@ -14,4 +15,56 @@ const port = 3005;
 
 app.listen(port, () => {
   console.log(`Listening at localhost:${port}`);
+});
+
+app.get('*', (reqProxy, resProxy) => {
+  const service = reqProxy.path.split('/')[2];
+  if (service === 'bookings') {
+    axios.get(`http://localhost:3000${reqProxy.path}`, {
+      params: reqProxy.query
+    })
+      .then((response) => {
+        resProxy.send(response.data);
+      });
+  } else if (service === 'dishes') {
+    axios.get(`http://localhost:3001${reqProxy.path}`)
+      .then((response) => {
+        resProxy.send(response.data);
+      });
+  } else if (service === 'restaurants') {
+    axios.get(`http://localhost:3003${reqProxy.path}`)
+      .then((response) => {
+        resProxy.send(response.data);
+      });
+  } else if (service === 'review_list') {
+    axios.get(`http://localhost:3002${reqProxy.path}`)
+      .then((response) => {
+        resProxy.send(response.data);
+      });
+  }
+});
+
+app.post('*', (reqProxy, resProxy) => {
+  const service = reqProxy.path.split('/')[2];
+  if (service === 'bookings') {
+    axios.post(`http://localhost:3000${reqProxy.path}`, reqProxy.body)
+      .then((response) => {
+        resProxy.send(response.data);
+      });
+  } else if (service === 'dishes') {
+    axios.get(`http://localhost:3001${reqProxy.path}`, reqProxy.body)
+      .then((response) => {
+        resProxy.send(response.data);
+      });
+  } else if (service === 'restaurants') {
+    axios.get(`http://localhost:3003${reqProxy.path}`, reqProxy.body)
+      .then((response) => {
+        resProxy.send(response.data);
+      });
+  } else if (service === 'review_list') {
+    axios.get(`http://localhost:3002${reqProxy.path}`, reqProxy.body)
+      .then((response) => {
+        resProxy.send(response.data);
+      });
+  }
 });
