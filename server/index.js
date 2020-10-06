12cb1b57b8bb93b16c51f7ab11c94b023701c7c2
Johnny Tang <email.john.e@gmail.com>
2020-10-05 17:07:12 -0700

    load features without data

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '/../public')));
